e25eec0b402c4cb7ff97f3d0af75c98cf19097c4
João Neiva <jg.neiva@gmail.com>
2023-07-09 19:54:27 -0300

    [Typescript] instalado TS e criação da database

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+console.log("O aplicativo foi iniciado");
+console.log(database_1.users);
+console.log(database_1.products);
